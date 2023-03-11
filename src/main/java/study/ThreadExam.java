8f69df7a9cbc383adb557ba63ddaa6435f92e2c5
aladindev <kjune0205@naver.com>
2023-03-11 17:57:58 +0900

    Thread

---
+++ src/main/java/study/ThreadExam.java
@@ -0,0 +1,15 @@
+package study;
+
+public class ThreadExam extends Thread {
+
+    String str;
+    public ThreadExam(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public void run() {
+
+        super.run();
+    }
+}
