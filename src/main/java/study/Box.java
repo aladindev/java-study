d6369005700f7ea5c98e3ac678fff158910f0f3d
aladindev <kjune0205@naver.com>
2023-03-11 16:36:24 +0900

    Generic

--- src/main/java/study/Box.java
+++ src/main/java/study/Box.java
@@ -1,6 +1,6 @@
 package study;
 
-public class Box<E> { // Box class는 가상 클래스 E를 받을 수 있도록 제네릭 선언 
+public class Box<E> { // Box class는 가상 클래스 E를 받을 수 있도록 제네릭 선언
     private E obj;
 
     public void setObj(E obj) {
--- src/main/java/study/GenericExam.java
+++ src/main/java/study/GenericExam.java
@@ -2,14 +2,18 @@ package study;
 
 public class GenericExam {
     public static void main(String[] args) {
-        Box box = new Box(); // 인스턴스화
-        box.setObj(new Object());
-        Object obj = box.getObj();
+//        Box box = new Box(); // 인스턴스화
+//        box.setObj(new Object());
+//        Object obj = box.getObj();
+//
+//        box.setObj("Example");
+//        String str = (String)box.getObj();
+//
+//        box.setObj(1);
+//        int value = (int)box.getObj();
 
-        box.setObj("Example");
-        String str = (String)box.getObj();
-
-        box.setObj(1);
-        int value = (int)box.getObj();
+        // Type instance를 명시함으로써 컴파일 단계에서 에러를 사전 방지할 수 있다.
+        Box<String> box = new Box<>();
+        box.setObj(new String("1"));
     }
 }
